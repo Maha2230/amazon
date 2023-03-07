f2753965b1682009e0b06329494bcf16cc656cfa
Maha2230 <wmadhav85@gmail.com>
2023-03-08 01:47:03 +0530

    this third code

--- main.py
+++ main.py
@@ -5,3 +5,14 @@ thislist = ["apple", "banana", "cherry", "apple", "cherry"]
 print(thislist)
 
 # this comes from master_branch
+class person:
+   def def__init__(self, name, age):
+    self.name=name
+    self.age=age
+    def myfun (self):
+        print("hellow my name is " +self.name)
+        p1=person ("jonn",36)
+        print(p1.age)
+        p1.age=40
+        print(p1.name)
+        print(p1.age)
